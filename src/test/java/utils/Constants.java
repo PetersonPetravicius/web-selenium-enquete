cc3f476cf380d6ae1aeacf500873770215597b4d
Peterson <peterson@rarolabs.com.br>
2021-02-28 18:12:23 -0300

    Alteração constants e remoção appTest

--- src/test/java/utils/Constants.java
+++ src/test/java/utils/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     public static String uriEnqueteBbb = "http://www.enquetebbb.com.br/bbb21/enquete/enquete-bbb-21-quem-deve-ser-o-quinto-eliminado";
 
     // VOTACAOBBB2021
-    public static String participante = "Pocah";                                        // Altere aqui o nome do participante para votar
+    public static String participante = "Lumena";                                        // Altere aqui o nome do participante para votar
     public static String encontrarParticipante = "//*[@alt=\""+participante+"\"]";
     public static String posicionarParticipanteTela = "arguments[0].scrollIntoView(true);";
     public static String popUpChato = "onesignal-slidedown-cancel-button";
